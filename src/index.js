5e654449fbb8e82f57aa0fc8dadb32a17514d4ce
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-12 17:42:10 -0800

    put redux in because I was looking at canvas.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
 import App from './components/App';
-import reducer from './reducers/reducer';
+// import reducer from './reducers/reducer';
 import './reset.css';
 
-const store = createStore(reducer);
+// const store = createStore(reducer);
+
+// render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// );
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
