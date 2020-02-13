13ec7d1330a16a0a218beee9127b81d5e24a330c
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-13 14:45:58 -0800

    hooked up redux devtools

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/App';
-import reducer from './reducers/reducer';
+import { reducer, initialState } from './reducers/reducer';
 import './reset.css';
 
-const store = createStore(reducer);
+const store = createStore(reducer, initialState, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 render(
   <Provider store={store}>
